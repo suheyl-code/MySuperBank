1c6099a0bd0d7449fc81b891f388b48f941c8473
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-02-08 19:35:07 +0300

    Add project files.

---
+++ MySuperBank/BankAccount.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySuperBank
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { 
+            get 
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            } 
+        }
+
+        private static int accountNumberSeed = 1234567890;
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+            Console.WriteLine($"Account Number <{this.Number}> was created for \"{this.Owner}\" with initial balance ${this.Balance}.");
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0) 
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive.");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawl(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawl must be positive.");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawl!");
+            }
+            var withdrawl = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawl);
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+            report.AppendLine("\tDate\t\tAmount\tNote");
+            foreach (var item in allTransactions)
+            {
+                report.AppendLine($"{item.Date}\t${item.Amount}\t{item.Notes}");
+            }
+            report.AppendLine($"\tBalance: \t${this.Balance}");
+            return report.ToString();
+        }
+    }
+}
---
+++ MySuperBank/Program.cs
@@ -0,0 +1,9 @@
+﻿using MySuperBank;
+
+var account1 = new BankAccount("John", 10000);
+var account2 = new BankAccount("Mirana", 999);
+
+account1.MakeDeposit(1, DateTime.UtcNow,"One dollar Deposit");
+account1.MakeWithdrawl(50, DateTime.UtcNow, "Xbox purchase");
+Console.WriteLine(account1.GetAccountHistory());
+Console.WriteLine(account2.GetAccountHistory());
